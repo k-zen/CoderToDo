b5556f3c58a4d114807bc01598716bb8e4fed66f
K-Zen <akc@apkc.net>
2017-03-02 11:48:24 -0300

    Minor modifications.

--- CoderToDo/iPhone/Controllers/AKAddViewController.swift
+++ CoderToDo/iPhone/Controllers/AKAddViewController.swift
@@ -15,12 +15,12 @@ class AKAddViewController: AKCustomViewController
     {
         self.presentView(controller: AKAddCategoryViewController(nibName: "AKAddCategoryView", bundle: nil),
                          taskBeforePresenting: { (presenterController, presentedController) -> Void in
-                            if let presenterController = presenterController as? AKAddViewController, let presentedController = presentedController as? AKAddCategoryViewController {
+                            if
+                                let presenterController = presenterController as? AKAddViewController,
+                                let presentedController = presentedController as? AKAddCategoryViewController {
                                 presentedController.project = presenterController.project
                             } },
                          dismissViewCompletionTask: { (presenterController, presentedController) -> Void in
-                            NSLog("=> INFO: \(type(of: presentedController)) MODAL PRESENTATION HAS BEEN DISMISSED...")
-                            
                             presenterController.dismissView(executeDismissTask: true) }
         )
     }
@@ -31,12 +31,12 @@ class AKAddViewController: AKCustomViewController
             try AKChecks.canAddTask(project: self.project)
             self.presentView(controller: AKAddTaskViewController(nibName: "AKAddTaskView", bundle: nil),
                              taskBeforePresenting: { (presenterController, presentedController) -> Void in
-                                if let presenterController = presenterController as? AKAddViewController, let presentedController = presentedController as? AKAddTaskViewController {
+                                if
+                                    let presenterController = presenterController as? AKAddViewController,
+                                    let presentedController = presentedController as? AKAddTaskViewController {
                                     presentedController.project = presenterController.project
                                 } },
                              dismissViewCompletionTask: { (presenterController, presentedController) -> Void in
-                                NSLog("=> INFO: \(type(of: presentedController)) MODAL PRESENTATION HAS BEEN DISMISSED...")
-                                
                                 presenterController.dismissView(executeDismissTask: true) }
             )
         }
@@ -53,26 +53,14 @@ class AKAddViewController: AKCustomViewController
         self.loadLocalizedText()
     }
     
-    override func viewDidAppear(_ animated: Bool)
-    {
-        super.viewDidAppear(animated)
-    }
-    
-    override func loadLocalizedText() {
-        super.loadLocalizedText()
-    }
-    
     // MARK: Miscellaneous
     func customSetup()
     {
-        super.additionalOperationsWhenTaped = { (gesture) -> Void in self.dismissView(executeDismissTask: false) }
+        super.additionalOperationsWhenTaped = { (gesture) -> Void in self.dismissView(executeDismissTask: true) }
         super.setup()
         
         // Custom L&F.
-        self.controlsContainer.layer.cornerRadius = GlobalConstants.AKViewCornerRadius
         self.controlsContainer.layer.borderWidth = CGFloat(GlobalConstants.AKDefaultBorderThickness)
-        self.controlsContainer.layer.borderColor = GlobalConstants.AKDefaultViewBorderBg.cgColor
-        self.addCategory.layer.cornerRadius = GlobalConstants.AKButtonCornerRadius
-        self.addTask.layer.cornerRadius = GlobalConstants.AKButtonCornerRadius
+        self.controlsContainer.layer.borderColor = UIColor.black.cgColor
     }
 }
